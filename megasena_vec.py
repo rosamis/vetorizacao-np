72a24acd0c71d1cff7cd38d8c5fea93eca2c84a8
Roberta <ro.samis.comp@gmail.com>
2020-12-21 11:17:08 -0300

    megasena

--- megasena_vec.py
+++ megasena_vec.py
@@ -1,5 +1,5 @@
 import numpy as np
-N_SIMULACOES = 100000000
+N_SIMULACOES = 10000000
 
 meu_jogo = np.random.randint(1,61,(N_SIMULACOES,6))
 jogo_sorteado = np.random.randint(1,61,(N_SIMULACOES,6))
@@ -11,3 +11,4 @@ resultado = meu_jogo == jogo_sorteado
 v_true = np.sum(resultado,axis=1)
 l_ganhou = v_true == 6
 ganhou = np.sum(l_ganhou)
+print(ganhou)
